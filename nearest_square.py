7d9c58e8ed69b0b26a481d2b8cf62e625b754464
Akhil S Kumar <akhilskumar120@gmail.com>
2021-01-10 14:37:58 +0530

    Step 5 completed

--- nearest_square.py
+++ nearest_square.py
@@ -1,11 +1,24 @@
 import math
+# Importing math for mathematical functions
 
 def nearest_square(n):
+    '''nearest_square
+
+    Parameters:
+    n (int): To get the nearest square below n
+
+    Returns:
+    int 0 : if n is less than 0
+    int i : nearest square number but less than n
+
+    '''
     if n<0:
         return 0
     else:
+        
+        # To find the square of the given number n
         root = math.sqrt(n)
-        for i in range(n,1,-1):
+        for i in range(n,-1,-1):
             if int(root)**2 ==i:
                 return i
             else:
